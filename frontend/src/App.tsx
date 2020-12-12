2bfd934a50e2644f13418ba2ce494e3baf9352dc
Roger Fernandes <rogerfernandes.aluno@unipampa.edu.br>
2020-12-11 23:48:41 -0300

    created Items

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,38 @@ import {Left, Right, Inputs,  Selects, Transitions, Content, SelectsContent} fro
 
 import  Select  from 'react-select';
 
-function App() {
+const App: React.FC = () => {
   const [newAlphabet, setNewAlphabet] = useState('');
   const [newStates, setNewStates] = useState('');
   const [newInitialState, setNewInitialState] = useState('');
   const [newFinalsStates, setNewFinalsStates] = useState('');
+  const [contentsItems, setContentsItems] = useState([
+    {state: '', alphabet: '', goState: ''}
+  ]);
+
 
   const alphabet = newAlphabet.split(';');
   const states = newStates.split(';');
 
+  function setContenstsItemsValue(position: number, field:string, value:string){
+    const updateContentsItems = contentsItems.map((contentItem, index) => {
+      if(index === position){
+        return { ...contentItem, [field]:value};
+      }
 
+      return contentItem;
+    })
+    setContentsItems(updateContentsItems);
+  }
 
+  function addNewContentItem(){
+    setContentsItems([
+      ...contentsItems,
+      {state: '', alphabet: '', goState: ''}
+    ]);
+  }
+
+  console.log(contentsItems);
 
   return (
     <>
@@ -60,33 +81,61 @@ function App() {
         <Transitions>
           <header>
             <strong></strong>
-            <button>+</button>
+            <button type="button" onClick={addNewContentItem}>+</button>
           </header>
           <Content>
-            {/* {contents.map((content) => (
-
-            ))} */}
-            <SelectsContent>  
-              <div>
-                <label>Estado</label>
-                <select />
-              </div>
-              <div>
-                <label>Alfabeto</label>
-                <select />
-              </div>
-              <div>
-                <label>Estado final</label>
-                <select />
-              </div>
-              <button>Deletar</button>
-            </SelectsContent>
+           {contentsItems.map((item, index) => {
+             return(
+                <SelectsContent key={index}>  
+                  <div>
+                    <label>Estado</label>
+                    <select 
+                      value={item.state}
+                      onChange={e => setContenstsItemsValue(index, 'state', e.target.value)}
+                    >
+                      {states.map((state) => (
+                        <option key={state} value={state}>{state}</option>
+                      ))}
+                    </select>
+                  </div>
+                  <div>
+                    <label>Alfabeto</label>
+                    <select
+                      value={item.alphabet}
+                      onChange={e => setContenstsItemsValue(index, 'alphabet', e.target.value)}
+                    >
+                      {alphabet.map((alphabet) => (
+                        <option key={alphabet} value={alphabet}>{alphabet}</option>
+                      ))}
+                    </select>
+                  </div>
+                  <div>
+                    <label>Estado final</label>
+                    <select
+                       value={item.goState}
+                       onChange={e => setContenstsItemsValue(index, 'goState', e.target.value)}
+                    >
+                      {states.map((state) => (
+                        <option key={`${state}f`} value={state}>{state}</option>
+                      ))}
+                    </select>
+                  </div>
+                </SelectsContent>
+             )
+           })}
+           
           </Content>
 
         </Transitions>
       </Left>
 
       <Right>
+        <header>
+          <input type="text" placeholder="palavra"/>
+          <button type="button">Confirmar</button>
+        </header>
+
+        <h1>Palavra null</h1>
 
       </Right>
     </>
